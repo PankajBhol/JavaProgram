25bc924d13b6f19e7f7b57bafc79b43bec6cd325
Pankaj Bhol <pankajbhol94@gmail.com>
2023-05-20 17:32:04 +0530

    This is Java Practice program files

---
+++ src/test/java/PrintNumWithoutLoop.java
@@ -0,0 +1,14 @@
+public class PrintNumWithoutLoop {
+
+    static int i=0;
+
+    public static void main(String[] args) {
+
+        if(i<=100) {
+            System.out.println(i++);
+            main(null);
+        }
+    }
+
+}
+
---
+++ src/test/java/RemoveJunkChar.java
@@ -0,0 +1,12 @@
+public class RemoveJunkChar {
+
+    public static void main(String[] args) {
+
+        String s="%$@#somanath*&#@jadhav";
+        //by using regular expression:
+        s=s.replaceAll("[^a-zA-z0-9]"," ");
+        System.out.println(s);
+    }
+
+}
+
